b2c4c1dc60cca404e76a4a59936e53ff6e34414a
PopovKM <k007pm@yandex.ru>
2024-04-14 17:58:38 +0500

    add practic_5

--- Practics/practic_4/main.py
+++ Practics/practic_4/main.py
@@ -3,8 +3,11 @@
 # а на выходе у нее — ZIP-архив со всеми PDF-файлами и извлеченным из них текстом 
 # (просто в `.txt`), которые можно получить по этому URL и его дочерним адресам 
 # (т.е. функция должна уметь ходить по ссылкам) 
-# (упаковать через модуль `zipfile` стандартной библиотеки).
+# (упаковать через модуль `zipfile` стандартной библиотеки)
 
+from pypdf import PdfReader
 
-
-
+reader = PdfReader("example.pdf")
+number_of_pages = len(reader.pages)
+page = reader.pages[0]
+text = page.extract_text()
